a6e6973dab17d4dcee1a8fce3a605f003cb45540
DiegoPinedaG <125941051+DiegoPinedaG@users.noreply.github.com>
2023-05-01 18:29:38 -0600

    Se sube tarea el 01/05/23
    Se sube tarea el 01/05/23

---
+++ Tarea9_PinedaDiego_POO_2258/src/main/java/ICO_POO_2258/Main.java
@@ -0,0 +1,9 @@
+package ICO_POO_2258;
+
+import ICO_POO_2258.iu.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        Principal ventana = new Principal("Hola Mundo AWT");
+    }
+}
