ed20e09c10a17020ce2a25734bac2339d1d1b895
DiegoPinedaG <125941051+DiegoPinedaG@users.noreply.github.com>
2023-05-05 20:31:18 -0600

    Se sube actividad extra el 05/05/23
    Se sube actividad extra el 05/05/23

---
+++ ActividadExtra1_PinedaDiego_2258/src/main/java/ICO_POO_2258/LeerArchivo.java
@@ -0,0 +1,18 @@
+package ICO_POO_2258;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.Buffer;
+
+public class LeerArchivo {
+    public static void main(String[] args) {
+        String ruta = "datos.txt";
+        try {
+            BufferedReader lector = new BufferedReader(new FileReader(ruta));
+        } catch (FileNotFoundException fnfe){
+            System.out.println("El archivo no existe!!");
+            System.out.println(fnfe.getMessage());
+        }
+    }
+}
