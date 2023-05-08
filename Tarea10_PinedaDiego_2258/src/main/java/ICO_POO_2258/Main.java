fae323670c3d7ae3aadc7076975baec8d22e3ae6
DiegoPinedaG <125941051+DiegoPinedaG@users.noreply.github.com>
2023-05-07 22:23:07 -0600

    Se sube tarea el 07/05/23
    Se sube tarea el 07/05/23

---
+++ Tarea10_PinedaDiego_2258/src/main/java/ICO_POO_2258/Main.java
@@ -0,0 +1,8 @@
+package ICO_POO_2258;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        VentanaSwing ventanaseing = new VentanaSwing("Centigrados a Farenheid");
+    }
+}
