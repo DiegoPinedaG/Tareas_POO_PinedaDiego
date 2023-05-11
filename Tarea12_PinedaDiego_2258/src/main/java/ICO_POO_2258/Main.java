f1f30ba83ffe2f895a2b06c6bb4f46eb3e39ef64
DiegoPinedaG <125941051+DiegoPinedaG@users.noreply.github.com>
2023-05-10 21:21:07 -0600

    Se sube tarea el 10/05/23
    Se sube tarea el 10/05/23

---
+++ Tarea12_PinedaDiego_2258/src/main/java/ICO_POO_2258/Main.java
@@ -0,0 +1,8 @@
+package ICO_POO_2258;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Ventana ventana = new Ventana("Ejamplo tabla MVC");
+    }
+}
